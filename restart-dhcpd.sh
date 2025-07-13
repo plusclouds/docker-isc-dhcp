b2bca13740f47e8444fc550ff210edd97531a869
Harun Barış Bulut <baris.bulut@plusclouds.com>
2025-07-13 16:30:40 +0300

    Update restart-dhcpd.sh

--- restart-dhcpd.sh
+++ restart-dhcpd.sh
@@ -5,6 +5,7 @@ touch dhcpd.leases
 # We are doing this because openresty, while running LUA, cannot read the environment variables.
 source /root/openresty.env
 
+# This is updated to take the config with predefined token
 curl {$API}/public/iaas/dhcp-servers/configuration/{$SERVER_ID} \
   -H "Authorization: {$ACCESS_TOKEN}" \
   -o /etc/dhcp/dhcpd.conf
